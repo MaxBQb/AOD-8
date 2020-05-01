461b9a13b67d7e29d99af9667a24b69c80b96b59
Inozpavel <Inozpavel@mail.ru>
2020-05-02 00:29:12 +0300

    Перенесен код из 7 работы, код разнесен на .cpp и .h

---
+++ Deque/Deque.cpp
@@ -0,0 +1,319 @@
+#include "Deque.h"
+
+using std::endl;
+using std::out_of_range;
+using std::cout;
+using std::cin;
+
+template <typename T>
+Deque<T>::Deque() : _head(nullptr), _nodesСount(0)
+{
+}
+
+template <typename T>
+Deque<T>::Deque(const Deque& obj)
+{
+	for (Node* current = obj._head;
+	     current != nullptr;
+	     current = current->next)
+	{
+		push_back(current->data);
+	}
+}
+
+// Добавление элемента (после pos)
+template <typename T>
+void Deque<T>::insert(T element, unsigned pos)
+{
+	if (_nodesСount == 0)
+		_head = new Node(element);
+	else
+	{
+		Node* current = _getElement(pos - 1);
+		current->next = new Node(element, current->next);
+	}
+	_nodesСount++;
+}
+
+// Добавление элемента (до pos)
+template <typename T>
+void Deque<T>::insert_before(T element, unsigned pos)
+{
+	if (_nodesСount == 0)
+		_head = new Node(element);
+	else if (pos == 0)
+		_head = new Node(element, _head);
+	else
+	{
+		Node* current = _getElement(pos - 1);
+		_getElement(pos - 1)->next = new Node(element, current->next);
+	}
+	_nodesСount++;
+}
+
+// Добавление элемента в конец
+template <typename T>
+void Deque<T>::push_back(T element)
+{
+	insert(element, _nodesСount);
+}
+
+// Добавление элемента в начало
+template <typename T>
+void Deque<T>::push_front(T element)
+{
+	insert_before(element, 0);
+}
+
+// Здесь переопределяется оператор []
+// В случае успеха вернёт элемент, иначе будет exception
+template <typename T>
+T& Deque<T>::operator[](unsigned index)
+{
+	Node* element = _getElement(index);
+	if (element == nullptr)
+		throw out_of_range("Ошибка! Некорректно задан индекс!");
+	return element->data;
+}
+
+// Обмен местами элементов first и second
+template <typename T>
+void Deque<T>::swap(unsigned firstPos, unsigned secondPos)
+{
+	T tempData = (*this)[firstPos];
+	(*this)[firstPos] = (*this)[secondPos];
+	(*this)[secondPos] = tempData;
+}
+
+// Удаление одного элемента по значению value
+// Вернёт true в случае успеха (нашёл)
+template <typename T>
+bool Deque<T>::remove(T value)
+{
+	Node* found = _search(value);
+	if (found == nullptr)
+		return false;
+	_remove_node(found, _getPrevious(found));
+	_nodesСount--;
+	return true;
+}
+
+// Удаление элемента на позиции pos
+// В случае, если это невозможно вернёт false
+template <typename T>
+bool Deque<T>::remove_at(unsigned pos)
+{
+	if (pos >= _nodesСount)
+		return false;
+	if (pos == 0)
+		_remove_node(_head);
+	else
+	{
+		Node* previous = _getElement(pos - 1);
+		if (previous == nullptr)
+			return false;
+		_remove_node(previous->next, previous);
+	}
+	_nodesСount--;
+	return true;
+}
+
+// Удаление всех элементов со значением value
+// Вернёт количество удалённых элементов
+template <typename T>
+unsigned Deque<T>::remove_all(T value)
+{
+	unsigned count = 0;
+	while (remove(value))
+		count++;
+	return count;
+}
+
+// Удаление последнего элемента
+// В случае, если это невозможно вернёт false
+template <typename T>
+bool Deque<T>::pop_back()
+{
+	return remove_at(_nodesСount - 1);
+}
+
+// Поиск одного элемента по значению
+// В случае успеха вернёт позицию первого элемента
+template <typename T>
+unsigned Deque<T>::find(T value, unsigned startPos)
+{
+	for (Node* current = _getElement(startPos);
+	     current != nullptr;
+	     current = current->next, startPos++)
+		if (current->data == value)
+			return startPos;
+	return NOT_FOUND; // unsigned_max
+}
+
+// Поиск всех элементов со значением value
+// Вернёт список позиций найденных элементов
+template <typename T>
+vector<unsigned> Deque<T>::find_all(T value)
+{
+	vector<unsigned> founds = {};
+	unsigned i = 0;
+	do
+	{
+		i = find(value, i);
+		if (i != NOT_FOUND)
+			founds.push_back(i);
+		else
+			break;
+		i++;
+	}
+	while (i < _nodesСount);
+	return founds;
+}
+
+// Изменяет значение найденного вхождения old_value на new_value
+// Вернёт true при успехе
+template <typename T>
+bool Deque<T>::update(T oldValue, T newValue)
+{
+	Node* current = _search(oldValue);
+	if (current == nullptr)
+		return false;
+	current->data = newValue;
+	return true;
+}
+
+// Изменяет значение всех найденных вхождения old_value на new_value
+// Вернёт количество замен
+template <typename T>
+unsigned Deque<T>::update_all(T oldValue, T newValue)
+{
+	unsigned i = 0;
+	Node* current = nullptr;
+	do
+	{
+		current = _search(oldValue, current);
+		if (current == nullptr)
+			return i;
+		current->data = newValue;
+		current = current->next;
+		i++;
+	}
+	while (i < _nodesСount);
+	return i;
+}
+
+// Вычисляет длинну списка (или возвращает кэшированную)
+template <typename T>
+unsigned Deque<T>::size()
+{
+	return _nodesСount;
+}
+
+// Выводит список на экран
+template <typename T>
+void Deque<T>::print_all()
+{
+	if (_nodesСount == 0)
+	{
+		cout << "Список пуст!" << endl;
+		return;
+	}
+	cout << "Содержание списка:" << endl;
+	unsigned i = 0;
+	for (Node* current = _head;
+	     current != nullptr;
+	     current = current->next, i++)
+	{
+		cout << "[" << i << "]\t" << current->data << endl;
+	}
+}
+
+// Удаляет все элементы из [start; stop)
+template <typename T>
+bool Deque<T>::erase(unsigned start, unsigned stop)
+{
+	if (start == stop) return false;
+	for (; start < stop; stop--)
+	{
+		if (!remove_at(start))
+			return false;
+	}
+	return true;
+}
+
+// Удаляет все элементы начиная со start
+template <typename T>
+bool Deque<T>::erase(unsigned start)
+{
+	return erase(start, _nodesСount);
+}
+
+// Удаляет все элементы
+template <typename T>
+bool Deque<T>::clear()
+{
+	return erase(0);
+}
+
+// Очистка памяти, удаление списка
+template <typename T>
+Deque<T>::~Deque()
+{
+	clear();
+}
+
+// Удаляет элемент, сохраняя целосность списка
+// previous = nullptr для удаления головного объекта
+template <typename T>
+void Deque<T>::_remove_node(Node* node, Node* previous)
+{
+	if (node == nullptr) return;
+	if (previous != nullptr)
+		previous->next = node->next;
+	else
+		_head = node->next;
+	if (node != nullptr)
+		delete node;
+}
+
+// Получает Node по индексу
+template <typename T>
+typename Deque<T>::Node* Deque<T>::_getElement(unsigned index)
+{
+	if (index >= _nodesСount)
+		return nullptr;
+	Node* element = _head;
+	for (unsigned i = 0; i < index; i++)
+		element = element->next;
+	return element;
+}
+
+// Ищет Node по value
+template <typename T>
+typename Deque<T>::Node* Deque<T>::_search(T value, Node* element)
+{
+	Node* current = element;
+	if (element == nullptr)
+		current = _head;
+	for (; current != nullptr; current = current->next)
+		if (current->data == value)
+			return current;
+	return nullptr;
+}
+
+// Получает предыдущий Node
+template <typename T>
+typename Deque<T>::Node* Deque<T>::_getPrevious(Node* node)
+{
+	Node* current = node;
+	Node* previous = nullptr;
+	for (Node* element = _head;
+	     element != nullptr;
+	     element = element->next)
+	{
+		if (element == current)
+			return previous;
+		previous = element;
+	}
+	return nullptr;
+}
--- Deque/Deque.h
+++ Deque/Deque.h
@@ -1,1 +1,63 @@
 #pragma once
+#ifndef LINKED_LIST_CPP
+#define LINKED_LIST_CPP
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+template <typename T>
+class Deque
+{
+public:
+	static unsigned const NOT_FOUND = ~0;
+	Deque();
+	void insert(T element, unsigned pos);
+	void insert_before(T element, unsigned pos);
+	void push_back(T element);
+	void push_front(T element);
+	T& operator[](unsigned index);
+	void swap(unsigned firstPos, unsigned secondPos);
+	bool remove(T value);
+	bool remove_at(unsigned pos);
+	unsigned remove_all(T value);
+	bool pop_back();
+	unsigned find(T value, unsigned startPos = 0);
+	vector<unsigned> find_all(T value);
+	bool update(T oldValue, T newValue);
+	unsigned update_all(T oldValue, T newValue);
+	unsigned size();
+	void print_all();
+	bool erase(unsigned start, unsigned stop);
+	bool erase(unsigned start);
+	bool clear();
+	Deque(const Deque& obj);
+	~Deque();
+private:
+	struct Node
+	{
+		T data;
+		Node* next;
+
+		Node(T data, Node* next = nullptr) : data(data), next(next)
+		{
+		}
+	};
+
+	Node* _head;
+	unsigned _nodesСount;
+	void _remove_node(Node* node, Node* previous = nullptr);
+	Node* _getElement(unsigned index);
+	Node* _search(T value, Node* element = nullptr);
+	Node* _getPrevious(Node* node);
+};
+
+/* Структура двусвязного списка:
+head -> node_1 -> ... -> node_n -> nullptr
+head так же node, но самый первый
+node указывающий на nullptr называется хвостом (tail)
+Linked List[0] = head
+Linked List[1] = head->next
+Linked List[2] = head->next->next
+*/
+#endif
